2191d7e27cbae452380b9c7293c9e99f6ba3e5c4
vmaurice <vincent.maurice@outlook.fr>
2022-03-13 14:01:19 +0100

    update

--- voiture.ino
+++ voiture.ino
@@ -1,7 +1,24 @@
-#include <RH_ASK.h>
-RH_ASK driver(2000,8,9);
+#include <Arduino_FreeRTOS.h>
+
+
+// define four Tasks
+TaskHandle_t TaskStopDirectionHandle;
+TaskHandle_t TaskDriveHandle;
+TaskHandle_t TaskReceiveHandle;
+TaskHandle_t TaskBuzzerHandle;
+
+void TaskStopDirection(void* pvParameters);
+void TaskDrive(void* pvParameters);
+void TaskReceive(void* pvParameters);
+void TaskBuzzer(void* pvParameters);
+
+
+#include <VirtualWire.h>
+
+#define RX_PIN 8
 
 #define DEBUG
+#define noDEBUG_STACK
 
 // Button
 #define bLeft 2
@@ -26,27 +43,40 @@ RH_ASK driver(2000,8,9);
 #define return_delay 25
 
 // Buzzer
-
-#define Buzzer 7
-
-bool buzz = false;
+#define Buzzer 10
 int intBuzzer = 500;
 bool poolBuzzer = true;
 
 // Receive data
 byte receive[3];
 
+// Elapsed time
 unsigned long elapsedTime;
+
+//Drive the car
+boolean driveStopBool = false;
+boolean centerBool = false;
+int lastButton = 0;
+
+boolean newValue = false;
+
 
 void setup() {
   // put your setup code here, to run once:
+  // put your setup code here, to run once:
   Serial.begin(115200);  // Debugging only
   while (!Serial);
-  if (!driver.init())
-    Serial.println("init failed");
+
+
+  vw_setup(2000);
+  vw_set_rx_pin(RX_PIN);
+  vw_rx_start();
+  
 
   
   // Initialisation
+
+  // Drive
   pinMode(PRO_A, OUTPUT);
   pinMode(PRO_R, OUTPUT);
   analogWrite(PRO_A, LOW);
@@ -56,167 +86,259 @@ void setup() {
   pinMode(DIR_D, OUTPUT);
   analogWrite(DIR_G, LOW);
   analogWrite(DIR_D, LOW);
+
+  // Stop
+  pinMode(STOP, OUTPUT);
+  pinMode(NO_STOP, OUTPUT);
+  digitalWrite(STOP, LOW);
+  digitalWrite(NO_STOP, LOW);
   
 
   // Button
   pinMode(bLeft, INPUT_PULLUP);
   pinMode(bCenter, INPUT_PULLUP);
   pinMode(bRight, INPUT_PULLUP);
-  
-  
 
-  pinMode(stop_delay, OUTPUT);
-  pinMode(return_delay, OUTPUT);
-  digitalWrite(stop_delay, LOW);
-  digitalWrite(return_delay, LOW);
+  receive[0] = (byte)128;
+  receive[1] = (byte)128;
+  receive[2] = (byte)1;
+
+  
+  // Now set up two Tasks to run independently.
+  xTaskCreate(
+    TaskBuzzer
+    ,  "Buzzer"  // A name just for humans
+    ,  64  // This stack size can be checked & adjusted by reading the Stack Highwater
+    ,  NULL
+    ,  configMAX_PRIORITIES - 3  // Priority, with 3 (configMAX_PRIORITIES - 1) being the highest, and 0 being the lowest.
+    ,  &TaskReceiveHandle );
+
+   xTaskCreate(
+    TaskReceive
+    ,  "Receive information"
+    ,  128  // Stack size
+    ,  NULL
+    ,  configMAX_PRIORITIES - 1  // Priority
+    ,  &TaskReceiveHandle );
+
+    xTaskCreate(
+    TaskDrive
+    ,  "Drive the car"
+    ,  160  // Stack size
+    ,  NULL
+    ,  configMAX_PRIORITIES - 2  // Priority
+    ,  &TaskDriveHandle );
+
+    xTaskCreate(
+    TaskStopDirection
+    ,  "Stop direction"
+    ,  128  // Stack size
+    ,  NULL
+    ,  configMAX_PRIORITIES - 3  // Priority
+    ,  &TaskStopDirectionHandle );
+    
+
+    //Serial.println("Start");
 }
 
+void loop() {}
 
 
-/*
- * Drive the car
- */
+// Stop direction
+void TaskStopDirection(void* pvParameters)
+{
 
-
-boolean driveStopBool = false;
-boolean centerBool = false;
-int lastButton = 0;
-
-
-void stopDirection() {
-  if (centerBool) {
-    if (digitalRead(bLeft) == LOW || digitalRead(bRight) == LOW ) {
-      Serial.println("Stop direction");
-      digitalWrite(DIR_G, LOW);
-      digitalWrite(DIR_D, LOW);
-      centerBool = false;
-    }
-  } else {
-     if (lastButton == 1 || lastButton == 2) {
-         if (digitalRead(bCenter) == LOW ||  1 == lastButton && digitalRead(bRight) == LOW || 2 == lastButton && digitalRead(bLeft) == LOW ) {
-          Serial.println("Stop direction");
-          digitalWrite(DIR_G, LOW);
-          digitalWrite(DIR_D, LOW);
-          lastButton = 0;
-        }
-     }
-     else {
-      if (digitalRead(bLeft) == LOW || digitalRead(bCenter) == LOW || digitalRead(bRight) == LOW) {
+  (void) pvParameters;
+  
+  for (;;)
+  {
+    //Serial.println("Stop direction");
+    
+    if (centerBool) {
+      if (digitalRead(bLeft) == LOW || digitalRead(bRight) == LOW ) {
         Serial.println("Stop direction");
         digitalWrite(DIR_G, LOW);
         digitalWrite(DIR_D, LOW);
+        centerBool = false;
+      }
+    } else {
+       if (lastButton == 1 || lastButton == 2) {
+           if (digitalRead(bCenter) == LOW ||  1 == lastButton && digitalRead(bRight) == LOW || 2 == lastButton && digitalRead(bLeft) == LOW ) {
+            Serial.println("Stop direction");
+            digitalWrite(DIR_G, LOW);
+            digitalWrite(DIR_D, LOW);
+            lastButton = 0;
+          }
+       }
+       else {
+        if (digitalRead(bLeft) == LOW || digitalRead(bCenter) == LOW || digitalRead(bRight) == LOW) {
+          Serial.println("Stop direction");
+          digitalWrite(DIR_G, LOW);
+          digitalWrite(DIR_D, LOW);
+        }
       }
     }
+    #ifdef DEBUG_STACK
+      Serial.print("- TASK ");
+      Serial.print(pcTaskGetName(TaskStopDirectionHandle));
+      Serial.print(", High Watermark: ");
+      Serial.print(uxTaskGetStackHighWaterMark(TaskStopDirectionHandle));
+      Serial.println();
+     #endif
+    vTaskDelay(1 / portTICK_PERIOD_MS);
   }
 }
 
 
-void drive (byte x,byte y, byte z) {
-  if (z == 0) {
-    buzz = true;
-  }
-  int pro_pwm;
-  if (x >= 120 and x <= 140) {
-    analogWrite(PRO_A, LOW);
-    analogWrite(PRO_R, LOW);
-    if (driveStopBool) {
-      digitalWrite(STOP, HIGH);
-      digitalWrite(NO_STOP, LOW);
-      delay(start_delay);
-      digitalWrite(STOP, LOW);
-      digitalWrite(NO_STOP, LOW);
-      delay(stop_delay);
-      digitalWrite(STOP, LOW);
-      digitalWrite(NO_STOP, HIGH);
-      delay(return_delay);
-      digitalWrite(STOP, LOW);
-      digitalWrite(NO_STOP, LOW);
-    }
-    driveStopBool = false;
-    
-  } else if (x > 140) {
-    if (PRO_R == HIGH) {
-      analogWrite(PRO_A, LOW);
-      analogWrite(PRO_R, LOW);
-      digitalWrite(STOP, HIGH);
-      digitalWrite(NO_STOP, LOW);
-      delay(start_delay);
-      digitalWrite(STOP, LOW);
-      digitalWrite(NO_STOP, LOW);
-      delay(stop_delay);
-      digitalWrite(STOP, LOW);
-      digitalWrite(NO_STOP, HIGH);
-      delay(return_delay);
-      digitalWrite(STOP, LOW);
-      digitalWrite(NO_STOP, LOW);
-    }
-    pro_pwm = (x - 133) * 2;
-    analogWrite(PRO_A, pro_pwm);
-    analogWrite(PRO_R, LOW);  
-    driveStopBool = true;
-  } else if (x < 120) {
-    if (PRO_A == HIGH) {
-      analogWrite(PRO_A, LOW);
-      analogWrite(PRO_R, LOW);
-      digitalWrite(STOP, HIGH);
-      digitalWrite(NO_STOP, LOW);
-      delay(start_delay);
-      digitalWrite(STOP, LOW);
-      digitalWrite(NO_STOP, LOW);
-      delay(stop_delay);
-      digitalWrite(STOP, LOW);
-      digitalWrite(NO_STOP, HIGH);
-      delay(return_delay);
-      digitalWrite(STOP, LOW);
-      digitalWrite(NO_STOP, LOW);
-    }
-    pro_pwm = (125 - x) * 2;  
-    analogWrite(PRO_A, LOW);
-    analogWrite(PRO_R, pro_pwm);
-    //analogWrite(PRO_P, pro_pmw);
-    driveStopBool = true;
-  }
+// Drive
+void TaskDrive(void* pvParameters)
+{
 
-
+  (void) pvParameters;
   
+  for (;;)
+  {
+    
+    byte x,y;
+    int pro_pwm;
 
-  // Direction
-  if (y >= 64 and y <= 192) {
-    centerBool = false;
-    if (digitalRead(bCenter) != LOW) {
-      if (digitalRead(bLeft) == LOW) {
-        analogWrite(DIR_D, 50);
-        analogWrite(DIR_G, LOW);
-        lastButton = 1;
-      } else if (digitalRead(bRight) == LOW) {
-        analogWrite(DIR_D, LOW);
-        analogWrite(DIR_G, 50); 
-        lastButton = 2;
+    if (newValue)
+    {
+
+      //Serial.println("Drive");
+
+      x = receive[0];
+      y = receive[1];
+  
+      if (x >= 120 and x <= 140) {
+        analogWrite(PRO_A, LOW);
+        analogWrite(PRO_R, LOW);
+        if (driveStopBool) {
+          digitalWrite(STOP, HIGH);
+          digitalWrite(NO_STOP, LOW);
+          delay(start_delay);
+          //vTaskDelay(start_delay / portTICK_PERIOD_MS);
+          digitalWrite(STOP, LOW);
+          digitalWrite(NO_STOP, LOW);
+          delay(stop_delay);
+          //vTaskDelay(stop_delay / portTICK_PERIOD_MS);
+          digitalWrite(STOP, LOW);
+          digitalWrite(NO_STOP, HIGH);
+          delay(return_delay);
+          //vTaskDelay(return_delay / portTICK_PERIOD_MS);
+          digitalWrite(STOP, LOW);
+          digitalWrite(NO_STOP, LOW);
+        }
+        driveStopBool = false;
+        
+      } else if (x > 140) {
+        if (PRO_R == HIGH) {
+          analogWrite(PRO_A, LOW);
+          analogWrite(PRO_R, LOW);
+          digitalWrite(STOP, HIGH);
+          digitalWrite(NO_STOP, LOW);
+          delay(start_delay);
+          //vTaskDelay(start_delay / portTICK_PERIOD_MS);
+          digitalWrite(STOP, LOW);
+          digitalWrite(NO_STOP, LOW);
+          delay(stop_delay);
+          //vTaskDelay(stop_delay / portTICK_PERIOD_MS);
+          digitalWrite(STOP, LOW);
+          digitalWrite(NO_STOP, HIGH);
+          delay(return_delay);
+          //vTaskDelay(return_delay / portTICK_PERIOD_MS);
+          digitalWrite(STOP, LOW);
+          digitalWrite(NO_STOP, LOW);
+        }
+        pro_pwm = (x - 133) * 2;
+        analogWrite(PRO_A, pro_pwm);
+        analogWrite(PRO_R, LOW);  
+        driveStopBool = true;
+      } else if (x < 120) {
+        if (PRO_A == HIGH) {
+          analogWrite(PRO_A, LOW);
+          analogWrite(PRO_R, LOW);
+          digitalWrite(STOP, HIGH);
+          digitalWrite(NO_STOP, LOW);
+          delay(start_delay);
+          //vTaskDelay(start_delay / portTICK_PERIOD_MS);
+          digitalWrite(STOP, LOW);
+          digitalWrite(NO_STOP, LOW);
+          delay(stop_delay);
+          //vTaskDelay(stop_delay / portTICK_PERIOD_MS);
+          digitalWrite(STOP, LOW);
+          digitalWrite(NO_STOP, HIGH);
+          delay(return_delay);
+          //vTaskDelay(return_delay / portTICK_PERIOD_MS);
+          digitalWrite(STOP, LOW);
+          digitalWrite(NO_STOP, LOW);
+        }
+        pro_pwm = (125 - x) * 2;  
+        analogWrite(PRO_A, LOW);
+        analogWrite(PRO_R, pro_pwm);
+        //analogWrite(PRO_P, pro_pmw);
+        driveStopBool = true;
       }
-    }
-  }
-  else {
-    if (digitalRead(bCenter) == LOW) {
-      centerBool = true;
-    }
-    if (y > 64) {
-      if (digitalRead(bRight) != LOW) {
-        //Serial.println("right");
-        analogWrite(DIR_D, 50);
-        analogWrite(DIR_G, LOW);
+    
+    
+     
+      // Direction
+      if (y >= 64 and y <= 192) {
+        centerBool = false;
+        if (digitalRead(bCenter) != LOW) {
+          if (digitalRead(bLeft) == LOW) {
+            analogWrite(DIR_D, 50);
+            analogWrite(DIR_G, LOW);
+            lastButton = 1;
+          } else if (digitalRead(bRight) == LOW) {
+            analogWrite(DIR_D, LOW);
+            analogWrite(DIR_G, 50); 
+            lastButton = 2;
+          }
+        }
       }
-    } else if (y < 192) {
-      if (digitalRead(bLeft) != LOW) {
-        //Serial.println("left");
-        analogWrite(DIR_G, 50);
-        analogWrite(DIR_D, LOW);
+      else {
+        if (digitalRead(bCenter) == LOW) {
+          centerBool = true;
+        }
+        if (y > 64) {
+          if (digitalRead(bRight) != LOW) {
+            //Serial.println("right");
+            analogWrite(DIR_D, 50);
+            analogWrite(DIR_G, LOW);
+          }
+        } else if (y < 192) {
+          if (digitalRead(bLeft) != LOW) {
+            //Serial.println("left");
+            analogWrite(DIR_G, 50);
+            analogWrite(DIR_D, LOW);
+          }
+        }
       }
+
+      newValue = false;
     }
+    #ifdef DEBUG_STACK
+      Serial.print("- TASK ");
+      Serial.print(pcTaskGetName(TaskDriveHandle));
+      Serial.print(", High Watermark: ");
+      Serial.print(uxTaskGetStackHighWaterMark(TaskDriveHandle));
+      Serial.println();
+     #endif
+    vTaskDelay(100 / portTICK_PERIOD_MS);
   }
 }
 
-void receiveData() {
-  if (driver.recv(receive, 3)) // Non-blocking
+// Receive data
+void TaskReceive(void* pvParameters)
+{
+  (void) pvParameters;
+  
+  for (;;)
+  {
+    //Serial.println("Received");
+    
+    if (vw_get_message(receive, 3)) // Non-blocking
     {
       // Message with a good checksum received, dump it.
       //driver.printBuffer("Got:", receive, 4);
@@ -230,20 +352,64 @@ void receiveData() {
         elapsedTime = millis();
        #endif
 
-       drive(receive[0], receive[1], receive[2]);
-
        //Serial.println("Receive");
+       newValue = true;
 
-       for (int i = 0; i < 100; i++) {
-        stopDirection();
-        delay(1);
-       }
+      #ifdef DEBUG_STACK
+        Serial.print("- TASK ");
+        Serial.print(pcTaskGetName(TaskReceiveHandle));
+        Serial.print(", High Watermark: ");
+        Serial.print(uxTaskGetStackHighWaterMark(TaskReceiveHandle));
+        Serial.println();
+       #endif
     }
+    vTaskDelay(50 / portTICK_PERIOD_MS);
+  }
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-  receiveData();
-  stopDirection();
-  yield();
+// Buzzer
+void TaskBuzzer(void* pvParameters)
+{
+
+  (void) pvParameters;
+
+  for (;;) 
+  {
+    //Serial.println("Buzzer");
+    
+    if (receive[2] == 0) {
+      int i;
+      if (poolBuzzer) {
+        for (i=intBuzzer; i<intBuzzer+20; i++) {
+          tone(Buzzer, i);
+          vTaskDelay(5 / portTICK_PERIOD_MS);
+        }
+        intBuzzer += 100;
+        if (intBuzzer == 1000)
+          poolBuzzer = false;
+      } else {
+        for (i=intBuzzer; i>intBuzzer-20; i--) {
+          tone(Buzzer, i);
+          vTaskDelay(5 / portTICK_PERIOD_MS);
+        }
+        intBuzzer -= 100;
+        if (intBuzzer == 500) {
+          poolBuzzer = true;
+          noTone(Buzzer);
+        }
+      }
+    } else {
+      intBuzzer = 0;
+      poolBuzzer = true;
+    }
+    #ifdef DEBUG_STACK
+      Serial.print("- TASK ");
+      Serial.print(pcTaskGetName(TaskBuzzerHandle));
+      Serial.print(", High Watermark: ");
+      Serial.print(uxTaskGetStackHighWaterMark(TaskBuzzerHandle));
+      Serial.println();
+     #endif
+    vTaskDelay(100 / portTICK_PERIOD_MS);
+    noTone(Buzzer);
+  }
 }
